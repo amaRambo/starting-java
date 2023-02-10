1ecf52966610fa884c03c151edf5291bdbe98b70
ilya <ilya_1_3_5@mail.ru>
2023-02-10 14:46:25 +0500

    rework started

--- hw/h1/task1.java
+++ hw/h1/task1.java
@@ -9,15 +9,23 @@ public class task1 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int n = getNumberByUser("Введите n: ");
-        int result = n*(n + 1)/2;
-        System.out.println(result);
+        
     }
     
 
-    public static int getNumberByUser(String text) {
+    public static int input(String text) {
         System.out.print(text);
         return input.nextInt();
     }
+
+    public static int work(String[] args) {
+        int n = input("Введите n: ");
+        int result = n*(n + 1)/2;
+        System.out.println(result);
+        return result;
+    }
     
+    void outputResult() {
+        System.out.println();
+    }
 }
