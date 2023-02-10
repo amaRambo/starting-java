1d871c2404d035f056ffd546beed748861c7bb59
ilya <ilya_1_3_5@mail.ru>
2023-02-10 14:33:48 +0500

    from classwork

--- seminars/lec1/program.java
+++ seminars/lec1/program.java
@@ -1,12 +1,30 @@
 package seminars.lec1;
 
+// /**
+//  * program
+//  */
+// public class program {
+
+//     public static void main(String[] args) {  
+//         System.out.println("hi");
+//     }
+// }
+
+import java.util.Scanner;
+
 /**
  * program
  */
 public class program {
 
-    public static void main(String[] args) {  
-        System.out.println("hi");
+    private static Scanner input = new Scanner(System.in);
+    public static int getNumberByUser(String text) {
+        System.out.print("Ведите n: ");
+        System.out.println("asd");
+        return input.nextInt();
     }
+
+    
+
 }
 
