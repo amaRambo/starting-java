e5b63e32d1dbfe0373e2d72735adc6f8c16fe433
ilya <ilya_1_3_5@mail.ru>
2023-02-10 14:34:21 +0500

    without return

--- hw/h1/task1.java
+++ hw/h1/task1.java
@@ -1,5 +1,23 @@
 package hw.h1;
+import java.util.Scanner;
 
+/**
+ * task1
+ */
 public class task1 {
+
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = getNumberByUser("Введите n: ");
+        int result = n*(n + 1)/2;
+        System.out.println(result);
+    }
+    
+
+    public static int getNumberByUser(String text) {
+        System.out.print(text);
+        return input.nextInt();
+    }
     
 }
