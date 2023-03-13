741edbf4e90e5810a602731fa3d11e100cb0cc8f
ilya <ilya_1_3_5@mail.ru>
2023-03-14 00:00:39 +0500

    asdas

---
+++ oop/seminars/sem4/App.java
@@ -0,0 +1,23 @@
+package oop.seminars.sem4;
+
+public class App {
+
+
+}
+
+// public static void main(String[] args){
+//     ObjectMapper mapper = new ObjectMapper();
+
+    
+//     Cat cat = new Cat("gil", 3, true);
+//     try {
+//         // Java объект в JSON файл
+//         mapper.writeValue(new File("/home/myuser/animals.json"), animal);
+
+//         // Java объект в JSON строку
+//         String jsonString = mapper.writeValueAsString(cat);
+//         System.out.println(jsonString);
+//     } catch (Exception e){
+//         System.out.println(e.getMessage());
+//     }
+// }
---
+++ oop/seminars/sem4/Cat.java
@@ -0,0 +1,16 @@
+package oop.seminars.sem4;
+
+public class Cat {
+    
+    protected String name;
+    protected int age;
+    protected boolean wool;
+
+    public Cat(String name, int age, boolean wool) {
+        this.name = name;
+        this.age = age;
+        this.wool = wool;
+    }
+
+    public String convertJson
+}
